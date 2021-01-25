ae4fc77ea647fd227f84c6f0513c19f7bbd32dbe
Adam Jonas <adamjonas25@gmail.com>
2021-01-25 14:21:22 +0000

    write test for Dealer#hand

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -1,7 +1,14 @@
 describe Dealer do
+  subject (:dealer) { Dealer.new }
   context '#initialize' do
     it 'initializes a new Dealer without arguments' do
       expect{ Dealer.new }.to_not raise_error
     end
   end
+
+  # context '#hand' do
+  #   it 'begins game empty' do
+  #     expect(dealer.hand).to eq []
+  #   end
+  # end
 end
