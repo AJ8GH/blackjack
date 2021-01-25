b94b97e19efd7cf434917b912b04829cf4f0dc54
Adam Jonas <adamjonas25@gmail.com>
2021-01-25 15:52:33 +0000

    add test for Player#hand

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,15 @@
 module BlackJack
   describe Player do
+    subject (:player) { Player.new }
     context '#initalize' do
       it 'creates new Player object' do
         expect { Player.new }.to_not raise_error
+      end
+    end
+
+    context '#hand' do
+      it 'returns the player hand' do
+        expect(player.hand).to eq []
       end
     end
   end
