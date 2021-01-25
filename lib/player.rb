9e4ed8e866479de482a2a9e560a253dadfc6cc98
Adam Jonas <adamjonas25@gmail.com>
2021-01-25 17:49:05 +0000

    define Player#score through inheritance from Dealer

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,13 @@
 module BlackJack
-  class Player
+  class Player < Dealer
     attr_accessor :hand
 
     def initialize
       @hand = []
+    end
+
+    def score
+      hand.map(&:value).map(&VALUES).sum
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,9 @@
 module BlackJack
   describe Player do
     subject (:player) { Player.new }
+    let (:jack) { Card.new(:jack, :hearts) }
+    let (:ten)  { Card.new(:ten, :clubs) }
+
     context '#initalize' do
       it 'creates new Player object' do
         expect { Player.new }.to_not raise_error
@@ -10,6 +13,17 @@ module BlackJack
     context '#hand' do
       it 'returns the player hand' do
         expect(player.hand).to eq []
+      end
+    end
+
+    context '#score' do
+      it 'returns 0 for empty hand' do
+        expect(player.score).to eq 0
+      end
+
+      it 'returns 20 for jack, ten' do
+        player.hand << jack << ten
+        expect(player.score).to eq 20
       end
     end
   end
