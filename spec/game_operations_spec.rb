75d9fd491467d3fbe7e646152a7dcb7a7d29db61
Adam Jonas <adamjonas25@gmail.com>
2021-01-26 18:58:42 +0000

    write test for play_again

--- spec/game_operations_spec.rb
+++ spec/game_operations_spec.rb
@@ -1,10 +1,19 @@
 module BlackJack
   describe GameOperations do
     subject (:game) { Game.new(Player.new) }
-    context 'initiate_deal' do
+
+    context '#initiate_deal' do
       it 'outputs expected statement' do
         allow_any_instance_of(Object).to receive(:gets) { "\n" }
         expect { game.initiate_deal }.to output("⏎ to deal\n").to_stdout
+      end
+    end
+
+    context '#play_again' do
+      it 'returns symbol of input' do
+        allow(STDOUT).to receive(:puts) { nil }
+        allow_any_instance_of(Object).to receive(:gets) { 'q' }
+        expect(game.play_again).to eq :q
       end
     end
   end
