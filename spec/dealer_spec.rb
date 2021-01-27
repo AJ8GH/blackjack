7870fe7a4fac191ebf9c32782664c16edbaefd05
Adam Jonas <adamjonas25@gmail.com>
2021-01-27 15:48:41 +0000

    add test for player methods

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -1,11 +1,8 @@
 module BlackJack
   describe Dealer do
     subject (:dealer) { Dealer.new }
-    let (:player) { Player.new }
     let (:king) { Card.new(:king, :hearts) }
-    let (:five) { Card.new(:five, :spades) }
     let (:ace) { Card.new(:ace, :hearts) }
-    let (:other_ace) { Card.new(:ace, :clubs) }
     let (:eight) { Card.new(:eight, :diamonds) }
 
     context '#initialize' do
@@ -29,79 +26,6 @@ module BlackJack
     context '#deck' do
       it 'has a deck on initialization' do
         expect(dealer.deck.class).to eq Deck
-      end
-    end
-
-    context '#calculate_score' do
-      it 'calculates 15 for king and 5' do
-        dealer.hand << king << five
-        expect(dealer.calculate_score).to eq 15
-      end
-
-      it 'updates the score' do
-        dealer.hand << king << five
-        dealer.calculate_score
-        expect(dealer.score).to eq 15
-      end
-    end
-
-    context '#ace_adjust' do
-      it 'subtracts 10 from score if dealer busts with one ace' do
-        dealer.hand << king << five << ace
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 16
-      end
-
-      it 'subtracts 20 from hand if dealer busts with 2 aces' do
-        dealer.hand << ace << other_ace << king << five
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 17
-      end
-
-      it 'does nothing if no aces in hand' do
-        dealer.hand << king << five
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 15
-      end
-
-      it 'subtracts 10 if the hand is 2 aces only' do
-        dealer.hand << ace << other_ace
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 12
-      end
-
-      it 'does nothing if score is 21' do
-        dealer.hand << ace << king
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 21
-      end
-
-      it 'subtracts 10 if the hand is 2 aces and an eight' do
-        dealer.hand << ace << other_ace << eight
-        dealer.calculate_score
-        dealer.ace_adjust
-        expect(dealer.score).to eq 20
-      end
-    end
-
-    context '#number of aces' do
-      it 'counts 0 for empty hand' do
-        expect(dealer.number_of_aces).to eq 0
-      end
-
-      it 'returns 1 for 1 ace' do
-        dealer.hand << ace << five
-        expect(dealer.number_of_aces).to eq 1
-      end
-
-      it 'returns 2 for 2 aces' do
-        dealer.hand << ace << five << other_ace << king
-        expect(dealer.number_of_aces).to eq 2
       end
     end
 
@@ -135,34 +59,6 @@ module BlackJack
         dealer.deck.cards << ace
         dealer.replenish_deck
         expect(dealer.deck.cards.count).to eq 1
-      end
-    end
-
-    context '#blackjack' do
-      it 'returns true if player has blackjack' do
-        player.hand << ace << king
-        player.calculate_score
-        expect(player.blackjack?).to be true
-      end
-
-      it 'returns false if no blackjack' do
-        dealer.hand << ace << eight
-        dealer.calculate_score
-        expect(dealer.blackjack?).to be false
-      end
-    end
-
-    context '#bust?' do
-      it 'returns true when bust' do
-        player.hand << king << eight << five
-        player.calculate_score
-        expect(player.bust?).to be true
-      end
-
-      it 'returns false when not bust' do
-        player.hand << eight << king
-        player.calculate_score
-        expect(player.bust?).to be false
       end
     end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,11 @@ module BlackJack
     subject (:player) { Player.new }
     let (:jack) { Card.new(:jack, :hearts) }
     let (:ten)  { Card.new(:ten, :clubs) }
+    let (:eight) { Card.new(:eight, :diamonds) }
+    let (:three) { Card.new(:three, :spades)}
+    let (:five) { Card.new(:five, :spades) }
+    let (:ace) { Card.new(:ace, :hearts) }
+    let (:other_ace) { Card.new(:ace, :clubs) }
 
     context '#initalize' do
       it 'creates new Player object' do
@@ -32,6 +37,122 @@ module BlackJack
         player.hand << jack << ten
         player.calculate_score
         expect(player.score).to eq 20
+      end
+    end
+
+    context '#bust?' do
+      it 'returns true when bust' do
+        player.hand << jack << eight << five
+        player.calculate_score
+        expect(player.bust?).to be true
+      end
+
+      it 'returns false when not bust' do
+        player.hand << eight << jack
+        player.calculate_score
+        expect(player.bust?).to be false
+      end
+    end
+
+    context '#twenty_one?' do
+      it 'returns true when player has 21 but not blackjack' do
+        player.hand << three << ten << eight
+        player.calculate_score
+        expect(player.twenty_one?).to be true
+      end
+    end
+
+    context '#blackjack' do
+      it 'returns true if player has blackjack' do
+        player.hand << ace << jack
+        player.calculate_score
+        expect(player.blackjack?).to be true
+      end
+
+      it 'returns false if no blackjack' do
+        player.hand << ace << eight
+        player.calculate_score
+        expect(player.blackjack?).to be false
+      end
+    end
+
+    context '#calculate_score' do
+      it 'calculates 15 for jack and 5' do
+        player.hand << jack << five
+        expect(player.calculate_score).to eq 15
+      end
+
+      it 'updates the score' do
+        player.hand << jack << five
+        player.calculate_score
+        expect(player.score).to eq 15
+      end
+    end
+
+    context '#ace_adjust' do
+      it 'subtracts 10 from score if player busts with one ace' do
+        player.hand << jack << five << ace
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 16
+      end
+
+      it 'subtracts 20 from hand if player busts with 2 aces' do
+        player.hand << ace << other_ace << jack << five
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 17
+      end
+
+      it 'does nothing if no aces in hand' do
+        player.hand << jack << five
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 15
+      end
+
+      it 'subtracts 10 if the hand is 2 aces only' do
+        player.hand << ace << other_ace
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 12
+      end
+
+      it 'does nothing if score is 21' do
+        player.hand << ace << jack
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 21
+      end
+
+      it 'subtracts 10 if the hand is 2 aces and an eight' do
+        player.hand << ace << other_ace << eight
+        player.calculate_score
+        player.ace_adjust
+        expect(player.score).to eq 20
+      end
+    end
+
+    context '#number of aces' do
+      it 'counts 0 for empty hand' do
+        expect(player.number_of_aces).to eq 0
+      end
+
+      it 'returns 1 for 1 ace' do
+        player.hand << ace << five
+        expect(player.number_of_aces).to eq 1
+      end
+
+      it 'returns 2 for 2 aces' do
+        player.hand << ace << five << other_ace << jack
+        expect(player.number_of_aces).to eq 2
+      end
+    end
+
+    context '#show_score' do
+      it 'returns score: score' do
+        player.hand << ten << jack
+        expect(player.show_score).to eq "score: 20\n\n"
       end
     end
   end
